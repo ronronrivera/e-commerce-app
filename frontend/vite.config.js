fd33b9f0b5f7ab55184b30279a9d4046be368b13
ronronrivera <ronaspe42@gmail.com>
2025-11-05 22:01:37 +0800

    redeploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     proxy: {
       "/api": {
         target: "http://localhost:8080",
       }
     }
-  }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  base: '/', // Explicitly set base path
 })
