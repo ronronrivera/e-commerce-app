2d4d7b9fec41f66d749181285c10faba89aedc3f
ronronrivera <ronaspe42@gmail.com>
2025-11-05 20:08:12 +0800

    deploying the website on vercel

--- backend/api/server.js
+++ backend/api/server.js
@@ -12,6 +12,8 @@ import couponsRoutes from "../routes/coupons.route.js"
 import paymentRoutes from "../routes/payment.routes.js";
 import analyticsRoutes from "../routes/analytics.routes.js";
 
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -36,7 +38,6 @@ if(process.env.NODE_ENV === "production"){
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
-
 /*
 try{
 	await connectDB();
@@ -48,6 +49,7 @@ catch(error){
 	console.log("Failed to connect to the database", error)
 	process.exit(1);
 }
+<<<<<<< HEAD
 */
 /*
 connectDB().then(() =>{
